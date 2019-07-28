460c3bce665bbc39a59a30f8d8bb9247187b955f
yangzq <yangzq19931003@outlook.com>
2019-07-28 22:12:45 +0800

    提交作业

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.UserMapper;
 import com.example.demo.entity.User;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,25 +14,36 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    @GetMapping("/user")
+    @GetMapping("/")
     public List<User> findAll() {
-        return null;
+        return userMapper.findAll();
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+        public List<User> findInPage(@RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "20") int pageSize) {
+        // startPage(pageNum, pageSize);
+        PageHelper.startPage(pageNum, pageSize);
+        return userMapper.findAll();
     }
 
     @GetMapping("/user/{userId}")
     public User findById(@PathVariable long userId) {
-        return null;
+        return userMapper.findById(userId);
     }
 
     @PostMapping("/user")
     public void insertUser(@RequestBody User user) {
+        userMapper.insertUser(user);
     }
 
     @PutMapping("/user/{userId}")
-    public void updateUser(@RequestBody User user,@PathVariable int userId) {
+    public void updateUser(@RequestBody User user,@PathVariable long userId) {
+        userMapper.updateUser(userId, user.getName());
     }
 
     @DeleteMapping("/user/{userId}")
     public void deleteById(@PathVariable long userId) {
+        userMapper.deleteUser(userId);
     }
 }
--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -1,17 +1,34 @@
 package com.example.demo.dao;
 
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserMapper {
+
     // 1. 实现查询所有用户
+    @Select("select * from user")
+    List<User> findAll();
 
     // 2. 根据用户id查询用户
+    @Select("select * from user where id = #{id}")
+    User findById(Long id);
 
     // 3. 添加新用户
+    // 返回的Integer值是变化的行数，@Options()会填充到实体 user 中。
+    @Insert("insert into user(id, name) value(#{id}, #{name})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insertUser(User user);
 
     // 4. 根据用户id删除用户
+    @Delete("delete from user where id = #{id}")
+    void deleteUser(Long id);
 
     // 5. 根据用户id更新用户
+    @Update("update user set name = #{name} where id = #{id}")
+    void updateUser(@Param("id") Long id, @Param("name") String name);
 
 }
